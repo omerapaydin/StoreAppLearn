c2f4f119210562a6fd17909264dcc51ccb440bb2
Ömer Apaydın <omerapaydin90@gmail.com>
2026-04-10 02:46:39 +0300

    IStoreRepository

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using StoreAppLearn.Data.Abstract;
 
 namespace StoreAppLearn.Controllers
 {
---
+++ Migrations/20260409234402_InitiaMigration.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace StoreAppLearn.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitiaMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: true),
+                    Description = table.Column<string>(type: "TEXT", nullable: true),
+                    Price = table.Column<decimal>(type: "TEXT", nullable: false),
+                    Category = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Category", "Description", "Name", "Price" },
+                values: new object[,]
+                {
+                    { 1, null, "güzel telefon", "Samsung S24", 50000m },
+                    { 2, null, "güzel telefon", "Samsung S25", 60000m },
+                    { 3, null, "güzel telefon", "Samsung S26", 70000m },
+                    { 4, null, "güzel telefon", "Samsung S27", 80000m },
+                    { 5, null, "güzel telefon", "Samsung S28", 90000m },
+                    { 6, null, "güzel telefon", "Samsung S29", 100000m }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
